d2290d0bfdf7fd45278b940cb1163f584c8c2750
ahul567 <rahulrajrollno.25tmp@gmail.com>
2025-12-22 21:05:57 +0530

    Reset the AuraGreen alignmentand use the desciption on the below

--- solar-system-main/src/AuraGreen/AuraBanner.jsx
+++ solar-system-main/src/AuraGreen/AuraBanner.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './AuraGreen.css';
-export default function AuraBanner() {
-  return (
-    <div>
 
+export default function AuraBanner(props) {
+  return (
+    <div className='Banner-Text'>
+      <h1 className='Banner-head'>{props.head}</h1>
+      <p className='banner-span'>{props.span}</p>
     </div>
   )
 }
--- solar-system-main/src/AuraGreen/AuraGreen.jsx
+++ solar-system-main/src/AuraGreen/AuraGreen.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import AuraGreen_img from '../assets/AuraGreen.svg';
+import AuraBanner from './AuraBanner.jsx';
 import './AuraGreen.css'
+const banner_database=[{
+ head:"Welcome to EcoGen!",
+ span:"your smart partner in building a cleaner, greener future"
+}];
 export default function AuraGreen() {
   return (
     <div className='Aura-Banner'>
-           <div>
+          
             <img className='Aura-img' src={AuraGreen_img} alt="" />
+            {
+              banner_database.map((value,index)=>(
+                <AuraBanner key={index} head={value.head} span={value.span}/>
+              ))}
            
-            </div>
         
     </div>
   )
