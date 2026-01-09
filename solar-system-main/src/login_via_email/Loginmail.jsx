62d038a7f2017a6e18d4c4f72027acf80ce5916f
ahul567 <rahulrajrollno.25tmp@gmail.com>
2026-01-09 21:24:45 +0530

    create mailbox

--- solar-system-main/src/login_via_email/Loginmail.jsx
+++ solar-system-main/src/login_via_email/Loginmail.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Loginmail.css'
 import AuraGreen from '../assets/AuraGreen.svg';
+import ic_mail from '../assets/ic_outline-email.svg';
+const database = {
+ [
+    icon: ic_mail,
+     text: "Enter Your Email";
+
+  ]
+}
 export default function Loginmail() {
   return (
     <div className='win-box'>
       <img className='AuraGreenImage' src={AuraGreen} alt="" />
+      <div className="mailbox">
+        <div className='sub-box-mail'></div>
+      </div>
     </div>
+
   )
 }
